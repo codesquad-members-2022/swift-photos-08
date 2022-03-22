a98624dc6c20eb999eb2b8b2e0df3980b325137f
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-03-22 14:03:15 +0900

    [#1] Feat: 40개 Cell 랜덤 색상으로 채우기

--- PhotoAlbum/PhotoAlbum/ViewController.swift
+++ PhotoAlbum/PhotoAlbum/ViewController.swift
@@ -1,19 +1,26 @@
-//
-//  ViewController.swift
-//  PhotoAlbum
-//
-//  Created by 이준우 on 2022/03/22.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
     }
 
+}
 
+extension ViewController: UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 40
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CustomCollectionViewCell", for: indexPath)
+        cell.backgroundColor = UIColor(red: Double.random(in: 0...1), green: Double.random(in: 0...1), blue: Double.random(in: 0...1), alpha: 1)
+        return cell
+    }
 }
 
