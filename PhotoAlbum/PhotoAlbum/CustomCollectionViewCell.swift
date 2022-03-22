48303c37277a99834630977f5a71acb931c0703c
Lee SeEun <sally4405@naver.com>
2022-03-22 15:55:00 +0900

    [#5] Feat: collectionView cell 사이즈 지정, 커스텀뷰와 클래스 생성 및 표시
    - 커스텀뷰 사이즈 조절 실패

---
+++ PhotoAlbum/PhotoAlbum/CustomCollectionViewCell.swift
@@ -0,0 +1,29 @@
+import Foundation
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setImageView()
+    }
+    
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    func setImageView() {
+        addSubview(imageView)
+        
+        imageView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        imageView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        imageView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        imageView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- PhotoAlbum/PhotoAlbum/ViewController.swift
+++ PhotoAlbum/PhotoAlbum/ViewController.swift
@@ -1,26 +1,32 @@
 import UIKit
 
 class ViewController: UIViewController, UICollectionViewDelegate {
-
+    let images = ["IMG_4092.jpg", "IMG_4092.jpg", "IMG_4092.jpg"]
+    
     @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.collectionView.delegate = self
         self.collectionView.dataSource = self
+        
+        self.collectionView.register(CustomCollectionViewCell.self, forCellWithReuseIdentifier: "CustomCollectionViewCell")
     }
 
 }
 
-extension ViewController: UICollectionViewDataSource{
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 40
+        return images.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CustomCollectionViewCell", for: indexPath)
-        cell.backgroundColor = UIColor(red: Double.random(in: 0...1), green: Double.random(in: 0...1), blue: Double.random(in: 0...1), alpha: 1)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CustomCollectionViewCell", for: indexPath) as! CustomCollectionViewCell
+
+        cell.imageView.image = UIImage(named: images[indexPath.row]) ?? UIImage()
+        
         return cell
     }
+
 }
 
