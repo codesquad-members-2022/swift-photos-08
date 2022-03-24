d0d47bb5b0284a67620ee77c5ef7abb32a0b126d
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-03-24 16:29:47 +0900

    [#8] Feat: ViewController, CustomPhotoManager에 NotificationCenter관련 선언 추가

--- PhotoAlbum/PhotoAlbum/CustomPhotoManager.swift
+++ PhotoAlbum/PhotoAlbum/CustomPhotoManager.swift
@@ -2,4 +2,17 @@ import Foundation
 import Photos
 
 class CustomPhotoManager: NSObject, PHPhotoLibraryChangeObserver{
+    
+    enum UserInfoKey: String{
+        case authorizationDeniedAlert = "authorizationDeniedAlert"
+        case alertTitle = "alertTitle"
+        case alertMessage = "alertMessage"
+        case actionTitle = "actionTitle"
+        case settingActionHandler = "settingActionHandler"
+    }
+    
+    struct NotificationName{
+        static let  authorizationDeniedAlert = Notification.Name("authorizationDeniedAlert")
+    }
+    
 }
