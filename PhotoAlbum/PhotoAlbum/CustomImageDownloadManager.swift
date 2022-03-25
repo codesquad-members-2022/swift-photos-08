4204db89f7a8e6f61e3642bc73c9b00b970096cc
Lee SeEun <sally4405@naver.com>
2022-03-25 14:23:27 +0900

    [#10] Feat: CustomImageDownloadManager 클래스 분리하여 작성
    - 번들에서 doodle 파일 불러오고 데이터로 변환하는 함수 작성
    - 데이터에 있는 이미지 url에서 이미지 다운받는 함수 작성

---
+++ PhotoAlbum/PhotoAlbum/CustomImageDownloadManager.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+class CustomImageDownloadManager {
+    private (set) var imageData: [Data] = []
+    
+    func parsingDoodleData() {
+        guard let fileLocation = Bundle.main.url(forResource: "doodle", withExtension: "json") else { return }
+        
+        do {
+            let data = try Data(contentsOf: fileLocation)
+            let doodleList = try JSONDecoder().decode([Doodle].self, from: data)
+            convertDoodleToImage((doodleList))
+        } catch {
+            print(error)
+        }
+    }
+    
+    private func convertDoodleToImage(_ list: [Doodle]) {
+        list.forEach {
+            let url = URL(string: $0.image)!
+            if let data = try? Data(contentsOf: url) {
+                imageData.append(data)
+            }
+        }
+    }
+    
+
+}
