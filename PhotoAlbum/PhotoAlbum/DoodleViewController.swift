eb3c0beeb509c604aff98f54bbcb81a1047ddfd2
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-03-25 16:58:51 +0900

    [#12] Feat: DoodleViewController 내부에 이미지 다운로드 및 보여주기 기능 추가
    - viewDidLoad() 호출 시에 이미지 다운로드 요청
    - 이미지 다운로드 완료 후 컬렉션 뷰 업데이트

--- PhotoAlbum/PhotoAlbum/DoodleViewController.swift
+++ PhotoAlbum/PhotoAlbum/DoodleViewController.swift
@@ -2,17 +2,49 @@ import UIKit
 
 class DoodleViewController: UIViewController {
     
+    @IBOutlet weak var collectionView: UICollectionView!
+    private var customImageDownloadManager: CustomImageDownloadManager = CustomImageDownloadManager()
+    private var downloadedImage: [UIImage] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
         self.view.backgroundColor = .darkGray
         self.navigationItem.title = "Doodle"
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Close", style: .done, target: self, action: #selector(dismissView))
+        
+        DispatchQueue.main.async {
+            DispatchQueue.global().sync {
+                self.customImageDownloadManager.parsingDoodleData()
+                self.downloadedImage = self.customImageDownloadManager.imageData.map { UIImage(data: $0) ?? UIImage() }
+            }
+            
+            self.collectionView.register(CustomDoodleCollectionViewCell.self, forCellWithReuseIdentifier: "CustomDoodleCollectionViewCell")
+            self.collectionView.delegate = self
+            self.collectionView.dataSource = self
+        }
+        
     }
     
     @objc func dismissView() {
         self.dismiss(animated: false)
     }
-    
 }
 
-
+extension DoodleViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return downloadedImage.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CustomDoodleCollectionViewCell", for: indexPath) as! CustomDoodleCollectionViewCell
+        cell.imageView.image = downloadedImage[indexPath.row]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: 110, height: 50)
+    }
+    
+}
