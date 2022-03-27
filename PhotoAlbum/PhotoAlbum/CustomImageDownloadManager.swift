be94b89b7c65a3429ad65198372e85d2f6ac9d70
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-03-27 23:03:09 +0900

    [#14] Refactoring: CustomImageDownloadManager.parseDoodleData() 내부 예외처리 세분화
    - 사용자 정의 예외  따로 열거형으로 정의
    - 각 케이스별 예외 발생시 error 수준 로그 남기도록 수정

--- PhotoAlbum/PhotoAlbum/CustomImageDownloadManager.swift
+++ PhotoAlbum/PhotoAlbum/CustomImageDownloadManager.swift
@@ -1,26 +1,60 @@
 import Foundation
+import OSLog
+
+enum ImageDownloadingError: Error{
+    case gettingJSONDataError
+    case parsingJSONDataError
+    case downloadingImageDataError
+}
 
 class CustomImageDownloadManager {
+    
     private (set) var imageData: [Data] = []
-
-    func parsingDoodleData() {
+    private var logger: Logger = Logger()
+    
+    func parseDoodleData(){
         guard let fileLocation = Bundle.main.url(forResource: "doodle", withExtension: "json") else { return }
+        do{
+            let data = try getJSONData(fileLocation: fileLocation)
+            let doodleList = try decodeJSONData(data: data)
+            try convertDoodleToImage(doodleList)
+        }catch ImageDownloadingError.gettingJSONDataError{
+            logger.error("JSONDataDownloadingException")
+        }catch ImageDownloadingError.parsingJSONDataError{
+            logger.error("JSONDataParsingException ")
+        }catch ImageDownloadingError.downloadingImageDataError{
+            logger.error("ImageDataDownloadingException")
+        }catch{
+            logger.error("UnDesignatedException : \(error.localizedDescription)")
+        }
         
-        do {
+    }
+    
+    private func getJSONData(fileLocation: URL) throws -> Data{
+        do{
             let data = try Data(contentsOf: fileLocation)
-            let doodleList = try JSONDecoder().decode([Doodle].self, from: data)
-            convertDoodleToImage(doodleList)
-        } catch {
-            print(error)
+            return data
+        }catch{
+            throw ImageDownloadingError.gettingJSONDataError
         }
     }
     
-    private func convertDoodleToImage(_ list: [Doodle]) {
-        list.forEach {
+    private func decodeJSONData(data: Data) throws -> [Doodle]{
+        do{
+            let doodleList = try JSONDecoder().decode([Doodle].self, from: data)
+            return doodleList
+        }catch{
+            throw ImageDownloadingError.parsingJSONDataError
+        }
+    }
+    
+    private func convertDoodleToImage(_ list: [Doodle]) throws {
+        try list.forEach {
+
             let url = URL(string: $0.image)!
-            if let data = try? Data(contentsOf: url) {
-                imageData.append(data)
-            }
+            guard let data = try? Data(contentsOf: url) else { throw ImageDownloadingError.downloadingImageDataError }
+            imageData.append(data)
         }
     }
 }
+
