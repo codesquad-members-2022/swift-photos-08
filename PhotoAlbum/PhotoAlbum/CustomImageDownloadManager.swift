e4cd84f642c589841e28b58e4decfe77754ddfe8
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-03-28 00:14:25 +0900

    [#14] Refactor: URLSession과 DispatchQueue를 사용해서 비동기로 이미지 다운로드 받도록 수정
    - Data()를 사용하지 않고 URLSession을 사용하여 GET 요청해서 이미지 데이터 받도록 수정
    - error 객체가 존재하거나 response 객체를 제대로 받지 못하는 경우 에러 로그 찍도록 수정
    - 400번대 응답일 경우에도 에러 로그 찍도록 수정
    - 정상적인 응답일 경우 응답 받은 데이터를 이미지 데이터에 추가

--- PhotoAlbum/PhotoAlbum/CustomImageDownloadManager.swift
+++ PhotoAlbum/PhotoAlbum/CustomImageDownloadManager.swift
@@ -4,7 +4,6 @@ import OSLog
 enum ImageDownloadingError: Error{
     case gettingJSONDataError
     case parsingJSONDataError
-    case downloadingImageDataError
 }
 
 class CustomImageDownloadManager {
@@ -19,11 +18,9 @@ class CustomImageDownloadManager {
             let doodleList = try decodeJSONData(data: data)
             try convertDoodleToImage(doodleList)
         }catch ImageDownloadingError.gettingJSONDataError{
-            logger.error("JSONDataDownloadingException")
+             logger.error("JSONDataDownloadingException")
         }catch ImageDownloadingError.parsingJSONDataError{
             logger.error("JSONDataParsingException ")
-        }catch ImageDownloadingError.downloadingImageDataError{
-            logger.error("ImageDataDownloadingException")
         }catch{
             logger.error("UnDesignatedException : \(error.localizedDescription)")
         }
@@ -49,12 +46,37 @@ class CustomImageDownloadManager {
     }
     
     private func convertDoodleToImage(_ list: [Doodle]) throws {
-        try list.forEach {
-
+        list.forEach {
             let url = URL(string: $0.image)!
-            guard let data = try? Data(contentsOf: url) else { throw ImageDownloadingError.downloadingImageDataError }
-            imageData.append(data)
+            var urlRequest = URLRequest(url: url)
+            urlRequest.httpMethod = "GET"
+            DispatchQueue.main.async {
+                self.sendURLRequest(urlRequest: urlRequest)
+            }
         }
+    }
+    
+    private func sendURLRequest(urlRequest: URLRequest){
+        URLSession.shared.dataTask(with: urlRequest){ data, response, error in
+            guard error == nil else {
+                self.logger.error("\(error.debugDescription)")
+                return
+            }
+            guard let data = data else {
+                self.logger.error("data not received")
+                return
+            }
+            guard let response = response as? HTTPURLResponse else {
+                self.logger.error("http response not received")
+                return
+            }
+            
+            if(response.statusCode >= 400){
+                self.logger.debug("detected response with 404 status \nurl : \(urlRequest)")
+            }else{
+                self.imageData.append(data)
+            }
+        }.resume()
     }
 }
 
