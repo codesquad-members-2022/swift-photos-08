6a7387190e7a7f0222bba6e6b5e7610e769656b1
Lee SeEun <sally4405@naver.com>
2022-03-29 12:07:57 +0900

    [#16] Feat: cell에 longPressGesture 추가

--- PhotoAlbum/PhotoAlbum/DoodleViewController.swift
+++ PhotoAlbum/PhotoAlbum/DoodleViewController.swift
@@ -4,7 +4,7 @@ class DoodleViewController: UIViewController {
     
     @IBOutlet weak var collectionView: UICollectionView!
     private var customImageDownloadManager: CustomImageDownloadManager = CustomImageDownloadManager()
-        
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -18,14 +18,25 @@ class DoodleViewController: UIViewController {
         self.view.backgroundColor = .darkGray
         self.navigationItem.title = "Doodle"
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Close", style: .done, target: self, action: #selector(dismissView))
-      
+        
         self.collectionView.register(CustomDoodleCollectionViewCell.self, forCellWithReuseIdentifier: "CustomDoodleCollectionViewCell")
         self.collectionView.delegate = self
         self.collectionView.dataSource = self
     }
     
+    func addLongPressGesture() {
+        let longPress = UILongPressGestureRecognizer(target: self, action: #selector(targetViewDidPress(_:)))
+        longPress.minimumPressDuration = 0.5
+        longPress.isEnabled = true
+        self.collectionView.addGestureRecognizer(longPress)
+    }
+    
     @objc func dismissView() {
         self.dismiss(animated: false)
+    }
+    
+    @objc func targetViewDidPress(_ gesture: UILongPressGestureRecognizer) {
+        gesture.view?.becomeFirstResponder()
     }
 }
 
@@ -39,6 +50,12 @@ extension DoodleViewController: UICollectionViewDataSource, UICollectionViewDele
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CustomDoodleCollectionViewCell", for: indexPath) as! CustomDoodleCollectionViewCell
         let imageData = self.customImageDownloadManager.getImageData(index: indexPath.row)
         cell.imageView.image = UIImage(data: imageData)
+        
+        let longPress = UILongPressGestureRecognizer(target: self, action: #selector(targetViewDidPress(_:)))
+        longPress.minimumPressDuration = 0.5
+        longPress.isEnabled = true
+        cell.addGestureRecognizer(longPress)
+        
         return cell
     }
     
@@ -46,4 +63,24 @@ extension DoodleViewController: UICollectionViewDataSource, UICollectionViewDele
         return .init(width: 110, height: 50)
     }
     
+    func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return action == NSSelectorFromString("saveImage")
+    }
+    
 }
+
+class CustomMenuItem: UIMenuItem {
+    var cell: CustomDoodleCollectionViewCell?
+    
+    convenience init(title: String, action: Selector, cell: CustomDoodleCollectionViewCell? = nil) {
+        self.init(title: title, action: action)
+        
+        self.cell = cell
+    }
+}
+
+
